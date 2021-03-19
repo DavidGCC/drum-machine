79cac8cc05f38e109086aad2f977e0aabbee738f
DavidG <datog309@gmail.com>
2021-03-19 22:13:55 +0400

    added slider for volume

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import { Slider } from "@material-ui/core";
 
 const sounds = [
     {
@@ -69,20 +72,94 @@ const sounds = [
     },
 ];
 
+const getSoundByKey = key => sounds.find(sound => sound.triggerCode === key);
+
+
+const DrumPad = ({ sound, playSound }) => {
+    return (
+        <div onClick={playSound} className="drum-pad">
+            <audio id={sound.id} src={sound.src} />
+            {sound.trigger}
+        </div>
+    );
+};
+
+const DrumPads = ({ sounds, playSound }) => {
+    return (
+        <div id="drum-pads">
+            {
+                sounds.map(sound => (
+                    <DrumPad key={sound.id} sound={sound} playSound={() => playSound(sound)} />
+                ))
+            }
+        </div>
+    );
+};
+
 const App = () => {
-    const playSound = (e) => {
-        e.preventDefault();
-        const sound = document.getElementById("1");
-        sound.currentTime = 0;
-        sound.play();
+    const [currentPad, setCurrentPad] = React.useState();
+    const [volume, setVolume] = React.useState(0.2);
+    const playSound = (sound) => {
+        setCurrentPad(sound);
+        const soundElem = document.getElementById(sound.id);
+        soundElem.currentTime = 0;
+        soundElem.volume = volume;
+        soundElem.play();
     };
 
+    const handleKeyDown = e => {
+        // e.preventDefault();
+        switch (e.keyCode) {
+            case 81:
+                playSound(getSoundByKey(81));
+                break;
+            case 87:
+                playSound(getSoundByKey(87));
+                break;
+            case 69:
+                playSound(getSoundByKey(69));
+                break;
+            case 65:
+                playSound(getSoundByKey(65));
+                break;
+            case 83:
+                playSound(getSoundByKey(83));
+                break;
+            case 68:
+                playSound(getSoundByKey(68));
+                break;
+            case 90:
+                playSound(getSoundByKey(90));
+                break;
+            case 88:
+                playSound(getSoundByKey(88));
+                break;
+            case 67:
+                playSound(getSoundByKey(67));
+                break;
+            default:
+                return null;
+        }
+    };
+
+    const handleVolumeChange = (e, newVolume) => {
+        e.preventDefault();
+        setVolume(newVolume);
+    };
+
+    React.useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+    });
+
     return (
-        <div>
-            <button className="btn btn-danger" onClick={playSound}>
-                <audio id="1" src="/Sounds/hi-hat-2.mp3"></audio>
-                play
-            </button>
+        <div id="drum-machine"  onKeyDown={handleKeyDown}>
+            <div>
+                <DrumPads sounds={sounds} playSound={playSound} volume={volume} />
+            </div>
+            <div className="utility">
+                <h3 id="display">{currentPad?.name}</h3>
+                <Slider value={volume} onChange={handleVolumeChange} min={0} max={1} step={0.01} />
+            </div>
         </div>
     );
 };
